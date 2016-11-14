67a53a689d541e9742467b85e12648a7701384bf
Nigel-Sultan <ns672@nau.edu>
2016-11-14 16:45:48 -0700

    Previous header file

---
+++ header.h
@@ -0,0 +1,37 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+
+typedef struct{
+  	int type; // 0 = camera, 1 = sphere, 2 = plane, 3 = light
+  	union {
+    	struct {
+      		double width;
+      		double height;
+    	} camera;
+    	struct {
+    		double diffuse_color[3];
+    		double specular_color[3];
+      		double position[3];
+      		double radius;
+    	} sphere;
+    	struct {
+    		double diffuse_color[3];
+    		double specular_color[3];
+      		double position[3];
+      		double normal[3];
+    	} plane;
+    	struct {
+    		double color[3];
+      		double position[3];
+      		double direction[3];
+      		double radial_a2;
+      		double radial_a1;
+      		double radial_a0;
+      		double angular_a0;
+      		double theta;
+      		double ns;
+    	} light;
+  	};
+} Object;
+
+#endif // HEADER_H_
